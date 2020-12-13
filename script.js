2901b870bf4111d84e73e421f71137248d865504
tushartiwari0172@gmail.com <tushartiwari0172@gmail.com>
2020-12-13 22:09:58 +0530

    feat-api-added
    backend done minions api added.

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-var btnElement = document.querySelector("#btntranslate");
 var inputElement = document.querySelector("#txt-input");
+var btnElement = document.querySelector("#btntranslate");
 var outputElement = document.querySelector("#output");
 
-
-function showClick()  {
-    console.log("input is", inputElement.value)
-    outputElement.innerText = "hello " + inputElement.value;
+var url = "https://api.funtranslations.com/translate/minion.json"
+function callToApi()  {
+    var optimizedUrl = url + "?text=" + inputElement.value;
+    // processing
+    fetch(optimizedUrl)
+    .then(response => response.json())
+    .then(json => show(json))
 };
-btnElement.addEventListener("click", showClick);
+function show(result) {
+    outputElement.innerText = result.contents.translated;
+}
+btnElement.addEventListener("click", callToApi);
