2a5019979d4bdcdb373e40cc7f9324791d020649
tushartiwari0172@gmail.com <tushartiwari0172@gmail.com>
2020-12-13 23:05:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,8 +9,14 @@ function callToApi()  {
     fetch(optimizedUrl)
     .then(response => response.json())
     .then(json => show(json))
+    .catch(errorHandler)
 };
+
 function show(result) {
     outputElement.innerText = result.contents.translated;
+}
+function errorHandler {
+    console.log("error occured!");
+    outputElement.innerText = "Something went wrong with Server! please try again later.";
 }
 btnElement.addEventListener("click", callToApi);
